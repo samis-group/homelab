bc0568f164db12d4c17fc80a495939506964eb14
Sami Shakir <th3cookie@gmail.com>
2022-05-29 20:17:28 +1000

    working k3s cluster with kube-vip and etcd

--- makefiles/proxmox-plays.mk
+++ makefiles/proxmox-plays.mk
@@ -9,10 +9,6 @@ proxmox:
 proxmox-v:
 	@ansible-playbook -vvv -i inventory/hosts.ini playbook_proxmox.yml
 
-# Provision proxmox vm's.
-proxmox-vms:
-	@ansible-playbook -i inventory/hosts.ini --tags "provision_vms" -e "provision_vms=true" playbook_proxmox.yml
-
 # Create Ubuntu VM Template in Proxmox.
 proxmox-vm-template:
 	@ansible-playbook -i inventory/hosts.ini --tags "create_vm_template" -e "create_vm_template=true" playbook_proxmox.yml
@@ -20,3 +16,9 @@ proxmox-vm-template:
 # Force (re)create/(re)download Ubuntu VM Template in Proxmox form public ubuntu cloud-init image. Essentially if you want to remake the iamge from scratch, make this target.
 proxmox-force-vm-template:
 	@ansible-playbook -i inventory/hosts.ini --tags "create_vm_template" -e "create_vm_template=true force_template_rebuild=true" playbook_proxmox.yml
+
+proxmox-provision-docker-vms:
+	@ansible-playbook -i inventory/hosts.ini playbook_proxmox.yml --limit docker
+
+proxmox-provision-k3s-vms:
+	@ansible-playbook -i inventory/hosts.ini playbook_proxmox.yml --limit k3s
