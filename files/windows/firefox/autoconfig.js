ae0ba3d94596faf276df2ec89ede339d1b6a0957
Sami Shakir <th3cookie@gmail.com>
2022-06-15 23:09:40 +1000

    firefox on windows pc setup

---
+++ files/windows/firefox/autoconfig.js
@@ -0,0 +1,6 @@
+// First line is ignored.  Leave a single line comment here.
+
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+// For treestyletab extension
+pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
--- makefiles/windows-plays.mk
+++ makefiles/windows-plays.mk
@@ -6,10 +6,7 @@ windows:	## 🖥 Main Windows play
 	@ansible-playbook -i inventory/hosts.ini playbook_windows.yml $(runargs)
 
 windows-runtags:	## 🖥 Run windows play tags
-	@ansible-playbook -i inventory/hosts.ini --tags "$(runargs)" -e "$(runargs)=true" main.yml --limit windows
-
-windows-test:	## 🖥 Run test_task on 'windows' hosts
-	@ansible-playbook -i inventory/hosts.ini --tags "test_task" -e "test_task=true" main.yml --limit windows $(runargs)
+	@ansible-playbook -i inventory/hosts.ini --tags "$(runargs)" -e "$(runargs)=true" playbook_windows.yml
 
 windows-chocolatey:	## 🖥 Run chocolatey tasks
-	@ansible-playbook -i inventory/hosts.ini --tags "chocolatey" -e "chocolatey=true" main.yml --limit windows
+	@ansible-playbook -i inventory/hosts.ini --tags "chocolatey" -e "chocolatey=true" playbook_windows.yml
