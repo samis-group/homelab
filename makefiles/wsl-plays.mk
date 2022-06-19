56a26103d43715c5434bf24fe76cf2a201fb2898
Sami Shakir <th3cookie@gmail.com>
2022-06-19 13:37:03 +1000

    stow

--- makefiles/wsl-plays.mk
+++ makefiles/wsl-plays.mk
@@ -7,3 +7,6 @@ wsl-versent:	## 🐧 Make Base + Versent WSL instance (Assumes you're running fr
 
 wsl-personal:		## 🐧 Make Base + Personal WSL instance (Assumes you're running from it, so go setup and clone this repo there)
 	@ansible-playbook -i inventory/hosts.ini playbook_wsl.yml playbook_wsl_personal.yml $(runargs)
+
+wsl-runtags:	## 🖥 Run WSL play tags
+	@ansible-playbook -i inventory/hosts.ini --tags "$(runargs)" -e "$(runargs)=true" playbook_wsl.yml playbook_wsl_personal.yml
