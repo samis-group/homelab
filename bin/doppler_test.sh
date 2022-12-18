06a13a6733098cf555fd5934928aebcc5429ffd5
Sami Shakir <th3cookie@gmail.com>
2022-12-18 20:01:53 +1100

    doppler test

---
+++ bin/doppler_test.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+echo "Building..."
+echo "Secrets supplied successfuly for $DOPPLER_PROJECT > $DOPPLER_CONFIG in $DOPPLER_ENVIRONMENT environment"
+echo "var.. $TEST_DOPPLER_SECRET"
