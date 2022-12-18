5c3ed76588038d3840d8489c80856b6707924425
Sami Shakir <th3cookie@gmail.com>
2022-12-18 19:56:39 +1100

    move ansible_user outside group_vars/all

--- bin/build.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Building..."
-echo "Secrets supplied successfuly for $DOPPLER_PROJECT > $DOPPLER_CONFIG in $DOPPLER_ENVIRONMENT environment"
-echo "$TEST_DOPPLER_SECRET"
