b49d3c4c737ba28ef4d6d5490c070d22b444de26
Sami Shakir <th3cookie@gmail.com>
2023-03-12 15:19:41 +1100

    working doppler tf - flux tf needs work

---
+++ provision/terraform/doppler/main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    doppler = {
+      # version = <latest version>
+      source = "DopplerHQ/doppler"
+    }
+  }
+}
+
+###
+### Setup the Doppler provider
+###
+
+variable "doppler_token" {
+  type = string
+}
+
+# The provider must always be specified with authentication
+provider "doppler" {
+  # Your Doppler token, either a personal or service token
+  doppler_token = var.doppler_token
+  # The token can be provided with the environment variable `DOPPLER_TOKEN` instead
+}
+
+###
+### Read Doppler secrets with the doppler_secrets data provider
+###
+
+# Mapped access to secrets
+data "doppler_secrets" "this" {
+  # Project and config are required if you are using a personal token
+  project = "homelab"
+  config = "dev_container"
+}
+
+output "all_secrets" {
+  # nonsensitive used for demo purposes only
+  value = nonsensitive(data.doppler_secrets.this.map)
+}
+
+output "domain_name" {
+  # Individual keys can be accessed directly by name
+  value = nonsensitive(data.doppler_secrets.this.map.DOMAIN_NAME)
+}
+
+output "vm_netmask" {
+  # Use `tonumber` and `tobool` to parse string values into Terraform primatives
+  value = nonsensitive(tonumber(data.doppler_secrets.this.map.VM_NETMASK))
+}
+
+output "json_parsing_values" {
+  # JSON values can be decoded direcly in Terraform
+  # e.g. FEATURE_FLAGS = `{ "AUTOPILOT": true, "TOP_SPEED": 130 }`
+  value = nonsensitive(jsondecode(data.doppler_secrets.this.map.TEST_JSON_DATA)["test"])
+}
+
+###
+### Create and modify Doppler secrets with the `doppler_secret` resource
+###
+
+# resource "random_password" "db_password" {
+#   length = 32
+#   special = true
+# }
+
+# resource "doppler_secret" "db_password" {
+#   project = "backend"
+#   config = "dev"
+#   name = "DB_PASSWORD"
+#   value = random_password.db_password.result
+# }
+
+# output "resource_value" {
+#   # Access the raw secret value
+#   value = nonsensitive(doppler_secret.db_password.value)
+# }
+
+# output "resource_computed" {
+#   # Access the computed secret value (if using Doppler secrets referencing)
+#   value = nonsensitive(doppler_secret.db_password.computed)
+# }
+
+###
+### Create and modify Doppler projects, environments, configs, and service tokens
+###
+
+# resource "doppler_project" "test_proj" {
+#   name = "my-test-project"
+#   description = "This is a test project"
+# }
+
+# resource "doppler_environment" "ci" {
+#   project = doppler_project.test_proj.name
+#   slug = "ci"
+#   name = "CI-CD"
+# }
+
+# resource "doppler_config" "ci_github" {
+#   project = doppler_project.test_proj.name
+#   environment = doppler_environment.ci.slug
+#   name = "ci_github"
+# }
+
+# resource "doppler_service_token" "ci_github_token" {
+#   project = doppler_project.test_proj.name
+#   config = doppler_config.ci_github.name
+#   name = "test token"
+#   access = "read"
+# }
+
+# output "token_key" {
+#   # Access the service token key
+#   value = nonsensitive(doppler_service_token.ci_github_token.key)
+# }
--- provision/terraform/flux/main.tf
+++ provision/terraform/flux/main.tf
@@ -1,9 +1,36 @@
+# https://registry.terraform.io/providers/fluxcd/flux/latest/docs
+
 terraform {
   required_providers {
     flux = {
-      source = "fluxcd/flux"
+      source  = "fluxcd/flux"
+      # version = ">= 0.11.0"
     }
   }
+}
+
+variable "gitlab_owner" {
+  description = "gitlab owner"
+  type        = string
+  default     = "th3cookie"
+}
+
+variable "gitlab_token" {
+  description = "gitlab token"
+  type        = string
+  sensitive   = true
+}
+
+variable "repository_url" {
+  description = "gitlab repository url"
+  type        = string
+  default     = "https://gitlab.com/sami-group/homelab"
+}
+
+variable "target_path" {
+  description = "flux sync target path"
+  type        = string
+  default     = "clusters/home"
 }
 
 provider "flux" {
@@ -11,6 +38,18 @@ provider "flux" {
     config_path = "~/.kube/config"
   }
   git = {
-    url = "https://gitlab.com/sami-group/homelab"
+    url    = var.repository_url
+    http = {
+      username = var.gitlab_owner
+      password = var.gitlab_token
+    }
   }
+}
+
+resource "flux_bootstrap_git" "fluxing" {
+  path   = var.target_path
+  components_extra = [
+    "image-reflector-controller",
+    "image-automation-controller",
+  ]
 }
